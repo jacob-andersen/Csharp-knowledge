b0f5adf009a3c613cdaea0e30c2044dfa8777206
Jacob Andersen <jacobma87@gmail.com>
2021-03-16 22:03:42 -0400

    first commit

---
+++ CounterApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CounterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Counter App");
+            Console.Write("Enter in a number: ");
+            int.TryParse(Console.ReadLine(), out int number);
+
+            if(number>0) {
+                Console.Write("Do you want to count Up or Down from that number? (Options: Up or Down): ");
+                string response = Console.ReadLine();
+
+                int counter = 0;
+
+                if (response == "Down") {
+                    counter = number;
+                }
+
+                for (int i=0;i<=number;i++) {
+                    if (response == "Up") {
+                        Console.WriteLine("Counting Up: " + counter);
+                        } else {
+                        Console.WriteLine("Counting Down: " + counter);
+                    }
+                    counter += (response == "Up") ? 1 : -1;
+                }
+
+            }
+        }
+    }
+}
